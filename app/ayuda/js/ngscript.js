beede068c331c8c8f1d413736fdae496ac69fd2a
garibo <garibokevin@gmail.com>
2015-08-24 12:50:45 -0500

    Agregado modulo de ayuda

---
+++ app/ayuda/js/ngscript.js
@@ -0,0 +1,18 @@
+(function(){
+
+	angular.module('ayudaApp', ['ngRoute','ngResource'])
+
+
+	.controller('perfilCtrl', function($scope, $http) 
+	{
+		$scope.nombrePerfil = "";
+		$http.get('http://localhost/administrador/app/ajustes/php/usn/')
+		.then(function(respuesta) {
+           $scope.nombrePerfil = respuesta.data.nombre;
+        }, function(error) {
+
+        });
+	});
+
+
+})();
---
+++ login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]> <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="es"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Entrar</title>
+  <link rel="stylesheet" href="assets/css/login.css">
+  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+  <form class="sign-up">
+    <h1 class="sign-up-title">Logueate en segundos</h1>
+    <input type="text" class="sign-up-input" placeholder="Correo electronico?" autofocus>
+    <input type="password" class="sign-up-input" placeholder="Contraseña">
+    <input type="submit" value="Entrar!" class="sign-up-button">
+  </form>
+
+
+</body>
+</html>
