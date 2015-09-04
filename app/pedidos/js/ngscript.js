81f05f09e9b35d6a3df062ee4608920cf0c19560
Kevin Garibo <garibokevin@gmail.com>
2015-09-04 18:09:34 -0500

    Eliminada directiva OnLastRepeat

--- app/pedidos/js/ngscript.js
+++ app/pedidos/js/ngscript.js
@@ -617,14 +617,6 @@
 	  }
 	})
 
-	.directive('onLastRepeat', function() {
-        return function(scope, element, attrs) {
-            if (scope.$last) setTimeout(function(){
-                scope.$emit('onRepeatLast', element, attrs);
-            }, 1);
-        };
-    })
-
     .directive('mapa', function() {
 	  return {
 	    restrict: 'E',
