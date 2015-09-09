d7513453a75010e203a54b2e1ab40530a888ac7b
Kevin Garibo <garibokevin@gmail.com>
2015-09-09 18:16:37 -0500

    Corregidos bugs de productos

--- login.php
+++ login.php
@@ -1,23 +1,60 @@
-<!doctype html>
-<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]> <html class="lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang="es"> <!--<![endif]-->
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Entrar</title>
-  <link rel="stylesheet" href="assets/css/login.css">
-  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="ThemeBucket">
+    <link rel="shortcut icon" href="assets/images/favicon.png">
+
+    <title>Login</title>
+
+    <!--Core CSS -->
+    <link href="assets/bs3/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-reset.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet" />
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]>
+    <script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
 </head>
-<body>
-  <form class="sign-up">
-    <h1 class="sign-up-title">Logueate en segundos</h1>
-    <input type="text" class="sign-up-input" placeholder="Correo electronico?" autofocus>
-    <input type="password" class="sign-up-input" placeholder="Contraseña">
-    <input type="submit" value="Entrar!" class="sign-up-button">
-  </form>
+
+  <body class="login-body">
+
+    <div class="container">
+
+      <form class="form-signin" action="index.html">
+        <h2 class="form-signin-heading">Entra ahora</h2>
+        <div class="login-wrap">
+            <div class="user-login-info">
+                <input type="text" class="form-control" placeholder="Email" autofocus>
+                <input type="password" class="form-control" placeholder="Contraseña">
+            </div>
+            <label class="checkbox">
+                <input type="checkbox" value="remember-me"> Recuerdame
+            </label>
+            <button class="btn btn-lg btn-login btn-block" type="submit">Logueate</button>
+        </div>
+
+      </form>
+
+    </div>
 
 
-</body>
+
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/bs3/js/bootstrap.min.js"></script>
+
+  </body>
 </html>
